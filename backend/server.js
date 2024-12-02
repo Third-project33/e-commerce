af871da576f6de0f327117ca4fa598f20a96b831
sayfmejri02 <mejrisaif2020@gmail.com>
2024-12-02 10:43:06 +0100

    pull

--- backend/server.js
+++ backend/server.js
@@ -13,24 +13,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
-// Removed Socket.IO related code
-// const { Server } = require("socket.io"); 
 
-// const io = new Server(server, {
-//     cors: {
-//         origin: 'http://localhost:3001',
-//         methods: ['GET', 'POST'],
-//         credentials: true
-//     }
-// });
-
-// io.on("connection", (socket) => {
-//     console.log("A user connected");
-//     // ... existing socket event handlers ...
-//     socket.on("disconnect", () => {
-//         console.log("A user disconnected");
-//     });
-// });
 
 require("./database/index.js");
 const PORT = 3001;
@@ -40,8 +23,7 @@ const productsRoute = require("./routes/AllProducts.js");
 const cartRoute = require("./routes/cart.js");
 const postsRoute = require("./routes/posts.js");
 
-// Removed Socket.IO related routes
-// app.use("/socket", io); // This line is also removed
+
 
 app.use("/brands", brandsroute);
 app.use("/user", userRoute);
